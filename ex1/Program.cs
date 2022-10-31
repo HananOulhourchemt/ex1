fec5d52d46c9708c255e93feb50725a8a82636b0
hanane oulhourchemt <hananeoulhourchemt@gmail.com>
2022-10-31 10:22:15 +0100

    Add project files.

---
+++ ex1/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ex1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int p = 0, i, nbr;
+
+            Console.WriteLine(" Entrez un nombre: ");
+            nbr=int.Parse(Console.ReadLine());
+
+            for (i=1; i<=nbr; i++)
+            {
+                if (nbr%i==0)
+                {
+                    p++;
+                }
+            }
+            if (p==2)
+            {
+                Console.WriteLine(" ce nombre est un nombre premier.");
+            }
+            else
+            {
+                Console.WriteLine(" ce nombre n'est pas un nombre premier.");
+            }
+            Console.ReadKey();
+        }
+    }
+}
